1d691961f0a8f827e6c179c0a89d414a70b11f7b
Lukman Okunade <31183676+lokunade@users.noreply.github.com>
2020-03-26 21:38:13 +0100

    JavaScriptTask
    My Javascript Task

---
+++ Task.js
@@ -0,0 +1,12 @@
+var myName = "Lukman";
+console.log(myName);
+
+
+var myCourses = ["HTML", "CSS", "JavaScript"];
+console.log (myCourses);
+
+for (let i = 0; i <= 200; i++) {
+    if (i % 2 === 0) {
+         console.log (i);
+    }
+};
